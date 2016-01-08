459d62b05d4d835774dd2da1a3b4f1d79d4a15c3
Xulin <tiemann9898@gmail.com>
2016-01-08 13:43:18 +0100

    new Customer generate and orders auto to assign

--- Step_1/app/code/local/Nextorder/Guesttoreg/Model/Cron.php
+++ Step_1/app/code/local/Nextorder/Guesttoreg/Model/Cron.php
@@ -7,7 +7,7 @@
  */
 class Nextorder_Guesttoreg_Model_Cron{
 
-    protected $cron_status = false;
+//    protected $cron_status = false;
 
     public function _readOrders(){
 
@@ -42,5 +42,52 @@ class Nextorder_Guesttoreg_Model_Cron{
             ->setPassword('testtest');
 
         $customer->save();
+        $this->_orderAssign($orderInkreId, $customer->getId());
+        $this->_setDefaultBillingAdress($billingID, $customer->getId());
+    }
+
+    protected function _setDefaultBillingAdress($billingID, $customerid){
+
+        $addressForOrder = Mage::getModel('sales/order_address')->load($billingID);
+        $billingAddress = array (
+            'prefix'     => $addressForOrder->getData('prefix'),
+            'firstname'  => $addressForOrder->getData('firstname'),
+            'lastname'   => $addressForOrder->getData('lastname'),
+            'street'     => array (
+                '0' =>  $addressForOrder->getStreet(1),
+                '1' => $addressForOrder->getStreet(2),
+                '3' => $addressForOrder->getStreet(3)
+            ),
+            'city'       => $addressForOrder->getData('city'),
+            'region'     => $addressForOrder->getData('region'),
+            'region_id'  => $addressForOrder->getData('region_id'),
+            'company'    => $addressForOrder->getData('company'),
+            'postcode'   => $addressForOrder->getData('postcode'),
+            'country_id' => $addressForOrder->getData('country_id'),
+            'telephone'  => $addressForOrder->getData('telephone'),
+            'fax'        => $addressForOrder->getData('fax'),
+        );
+        $customAddress   = Mage::getModel('customer/address');
+        $customAddress->setData($billingAddress)
+            ->setCustomerId($customerid) // this is the most important part
+            ->setIsDefaultBilling('1')  // set as default for billing
+            ->setIsDefaultShipping('1') // set as default for shipping
+            ->setSaveInAddressBook('1');
+        $customAddress->save();
+
+    }
+
+    protected function _orderAssign($orderInkreId, $customerid){
+
+        $order = Mage::getModel("sales/order")->loadByIncrementId($orderInkreId);
+        $storeId = $order->getStoreId();
+        $websiteId = Mage::getModel('core/store')->load($storeId)->getWebsiteId();
+        $customer = Mage::getModel("customer/customer")->load($customerid);
+        $customer->setWebsiteId($websiteId);
+        $order->setCustomerId($customerid);
+        $order->setCustomerIsGuest(0);
+        $order->setCustomerGroupId($customer->getData('group_id'));
+        $order->addStatusHistoryComment('Generiert von Gast Bestellung(Neue Kunden)');
+        $order->save();
     }
 }
